84532cfcb800cae8ed7d011955fe09452fba8f1c
Julien ichard WCS <julien.y.richard@gmail.com>
2022-11-07 12:16:02 +0100

    creation du composant nav bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import NavBar from './components/NavBar';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>Ma beers discovery</h1>
     </div>
   );
