807401df00153f3bf4423a9edfd4f0b718ed8133
Julien RICHARD <julien.y.richard@gmail.com>
2023-05-23 15:25:09 +0200

    Changing the nameof the props on the state from Id to id

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from './pages/Home';
 import OneProduct from './pages/OneProduct';
 
 function App() {
-  const [page, setPage] = useState({ name: "Home", Id: null })
+  const [page, setPage] = useState({ name: "Home", id: null })
 
   return (
     <div className="App">
