826a4d2296f9c937af160fe97f66b7476c3200ac
Julien RICHARD <julien.y.richard@gmail.com>
2023-05-23 15:29:24 +0200

    Chnaging the value of name in the See all my beers button from allProduct to AllProduct

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = ({ setPage }) => {
       <h1 className="title is-1">My best beers, by me only</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate nihil temporibus cumque, inventore totam doloremque esse doloribus incidunt quam id iusto tempora dolorum qui aliquam earum vel illo? Esse, temporibus.
         Exercitationem temporibus qui ipsam hic consectetur quo sint minima eum veritatis, ipsum veniam recusandae a repellendus dolorum quos et suscipit nam. Consequuntur quibusdam, ducimus obcaecati eos aut atque rem temporibus.</p>
-      <button className="button is-black m-5" onClick={() => setPage({ name: "allProduct", id: null })}>See All my beers now !!!</button>
+      <button className="button is-black m-5" onClick={() => setPage({ name: "AllProduct", id: null })}>See All my beers now !!!</button>
       <div className="columns m-5">
         {beers.slice(1, 5).map((beer) => {
           return <button className="column m-2" key={beer.id} onClick={() => setPage({ name: "OneProduct", id: beer.id })} >
